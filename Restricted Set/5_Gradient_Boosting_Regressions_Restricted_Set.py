4b61fe9d0c8941c770bf59c06a73482816a26760
Niccolo' Gentile <120027710+Nicklg92@users.noreply.github.com>
2023-04-28 16:55:03 +0200

    Update 5_Gradient_Boosting_Regressions_Restricted_Set.py

--- Restricted Set/5_Gradient_Boosting_Regressions_Restricted_Set.py
+++ Restricted Set/5_Gradient_Boosting_Regressions_Restricted_Set.py
@@ -17,9 +17,9 @@ scaler = StandardScaler()
 COMMENTS
 
 This is the fifth script of the Restricted Set producing the results in 
-"Machine Learning in the Prediction of Human
-Wellbeing", joint first authorship by Oparina, E., Kaiser, C., 
-and Gentile, N., et al.    
+"Human Wellbeing and Machine Learning" by Ekaterina Oparina* (r) Caspar Kaiser* (r) Niccolò Gentile*; 
+Alexandre Tkatchenko, Andrew E. Clark, Jan-Emmanuel De Neve 
+and Conchita D'Ambrosio (* equal first authors in random order).   
 
 Aim of this script is to fit and predict via Gradient Boosting
 on the Restricted set of variables, cross-sectionally on the 
