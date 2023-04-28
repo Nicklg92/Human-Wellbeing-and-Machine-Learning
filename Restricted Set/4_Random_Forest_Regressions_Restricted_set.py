5e3390407bdfc5f9c6588893837822ca695122e7
Niccolo' Gentile <120027710+Nicklg92@users.noreply.github.com>
2023-04-28 16:54:50 +0200

    Update 4_Random_Forest_Regressions_Restricted_set.py

--- Restricted Set/4_Random_Forest_Regressions_Restricted_set.py
+++ Restricted Set/4_Random_Forest_Regressions_Restricted_set.py
@@ -17,9 +17,9 @@ np.random.seed(1123581321)
 COMMENTS
 
 This is the fourth script of the Restricted Set producing the results in 
-"Machine Learning in the Prediction of Human
-Wellbeing", joint first authorship by Oparina, E., Kaiser, C., 
-and Gentile, N., et al.    
+"Human Wellbeing and Machine Learning" by Ekaterina Oparina* (r) Caspar Kaiser* (r) Niccolò Gentile*; 
+Alexandre Tkatchenko, Andrew E. Clark, Jan-Emmanuel De Neve 
+and Conchita D'Ambrosio (* equal first authors in random order).   
 
 Aim of this script is to fit and predict via Random Forests
 on the restricted set of variables, cross-sectionally on the 
